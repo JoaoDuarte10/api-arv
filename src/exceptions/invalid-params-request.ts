02458f871a68f0ecd5a90524dd79dc3dbc53fef8
JoaoDuarte10 <joaoduarte7341@gmail.com>
2022-11-28 15:21:12 -0300

    refactor(sales): Add handleController interceptor in controller

--- src/exceptions/invalid-params-request.ts
+++ src/exceptions/invalid-params-request.ts
@@ -2,7 +2,7 @@ import { BaseHttpException } from './base-http';
 import { HttpStatus } from '@nestjs/common';
 
 export class InvalidParamsRequestException extends BaseHttpException {
-  constructor(message: string, details: any[]) {
+  constructor(message: string, details?: any[]) {
     super(
       HttpStatus.BAD_REQUEST,
       'InvalidParamsRequestException',
--- src/modules/sales/sales.controller.ts
+++ src/modules/sales/sales.controller.ts
@@ -13,6 +13,8 @@ import { SalesService } from './sales.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RequestType } from '../../types/request';
 import { SalesDTO } from './sales.dto';
+import { handleController } from '../../infra/http/handle-controller';
+import { InvalidParamsRequestException } from '../../exceptions/invalid-params-request';
 
 @Controller('sales')
 export class SalesController {
@@ -21,7 +23,7 @@ export class SalesController {
   @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Req() req: RequestType, @Body() sale: SalesDTO): Promise<void> {
-    try {
+    return handleController(async () => {
       const payload = {
         idusers: req.user.idusers,
         idclients: sale.idclients,
@@ -32,81 +34,71 @@ export class SalesController {
         paymentDate: sale.paymentDate,
       };
       await this.salesService.create(payload);
-    } catch (error) {
-      throw new HttpException(
-        {
-          statusCode: error.getStatusCode(),
-          details: error.getDetails(),
-        },
-        error.getStatusCode(),
-      );
-    }
+    });
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('date')
   async findByDate(@Req() req: RequestType): Promise<SalesDTO[]> {
-    const date = req.query.date;
-    if (!date) {
-      throw new HttpException('Date is invalid', HttpStatus.BAD_REQUEST);
-    }
-    return await this.salesService.findByDate(req.user.idusers, date);
+    return handleController(async () => {
+      const date = req.query.date;
+      if (!date) {
+        throw new InvalidParamsRequestException('Date is invalid');
+      }
+      return await this.salesService.findByDate(req.user.idusers, date);
+    });
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('period')
   async findByPeriod(@Req() req: RequestType): Promise<SalesDTO[]> {
-    const date1 = req.query.date1;
-    const date2 = req.query.date2;
-    if (!date1 || !date2) {
-      throw new HttpException('Date is invalid', HttpStatus.BAD_REQUEST);
-    }
-    return await this.salesService.findByPeriod(req.user.idusers, date1, date2);
+    return handleController(async () => {
+      const date1 = req.query.date1;
+      const date2 = req.query.date2;
+      if (!date1 || !date2) {
+        throw new InvalidParamsRequestException('The range date is invalid');
+      }
+      return await this.salesService.findByPeriod(
+        req.user.idusers,
+        date1,
+        date2,
+      );
+    });
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('client')
   async findByClient(@Req() req: RequestType): Promise<SalesDTO[]> {
-    const idclients = req.query.idclients;
-    if (!idclients) {
-      throw new HttpException('Idclient is invalid', HttpStatus.BAD_REQUEST);
-    }
-    return await this.salesService.findByClient(req.user.idusers, idclients);
+    return handleController(async () => {
+      const idclients = req.query.idclients;
+      if (!idclients) {
+        throw new InvalidParamsRequestException('Idclient is invalid');
+      }
+      return await this.salesService.findByClient(req.user.idusers, idclients);
+    });
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete()
   async delete(@Req() req: RequestType): Promise<void> {
-    const idsales = Number(req.query.idsales);
-    if (!idsales) {
-      throw new HttpException('Idsales is invalid', HttpStatus.BAD_REQUEST);
-    }
-    await this.salesService.delete(req.user.idusers, idsales);
+    return handleController(async () => {
+      const idsales = Number(req.query.idsales);
+      if (!idsales) {
+        throw new InvalidParamsRequestException('Idsales is invalid');
+      }
+      await this.salesService.delete(req.user.idusers, idsales);
+    });
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('register-payment')
   async registerPayment(@Req() req: RequestType): Promise<void> {
-    const idsales = Number(req.query.idsales);
-    if (!idsales) {
-      throw new HttpException('Idsales is invalid', HttpStatus.BAD_REQUEST);
-    }
-    try {
-      await this.salesService.registerPayment(req.user.idusers, idsales);
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new HttpException(
-          error.message,
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+    return handleController(async () => {
+      const idsales = Number(req.query.idsales);
+      if (!idsales) {
+        throw new HttpException('Idsales is invalid', HttpStatus.BAD_REQUEST);
       }
-      throw new HttpException(
-        {
-          statusCode: error.getStatusCode(),
-          details: error.getDetails(),
-        },
-        error.getStatusCode(),
-      );
-    }
+      await this.salesService.registerPayment(req.user.idusers, idsales);
+    });
   }
 }
