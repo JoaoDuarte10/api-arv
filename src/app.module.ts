7b32193878a416c682c84b1b4abd9b6d644ece9a
JoaoDuarte10 <joaoduarte7341@gmail.com>
2022-11-29 18:31:25 -0300

    feat: Add prefix 'api' in all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { loadEnvs } from './config/load-envs';
 import { configProviders } from './providers/config-provider';
 import { Modules } from './modules/index';
 import { HealthCheckController } from './controllers/health-check';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -12,6 +13,14 @@ import { HealthCheckController } from './controllers/health-check';
       load: [loadEnvs],
     }),
     ...Modules,
+    RouterModule.register([
+      ...Modules.map((module) => {
+        return {
+          path: 'api',
+          module,
+        };
+      }),
+    ]),
   ],
   controllers: [HealthCheckController],
   providers: [...configProviders],
