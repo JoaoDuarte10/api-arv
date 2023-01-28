f2aaaca7ea752c9cc1cfc08e136b4eb36490c2d5
JoaoDuarte10 <joaoduarte7341@gmail.com>
2023-01-28 09:03:59 -0300

    feat: Add swagger stats

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfig } from './config/config';
 import { description, version } from '../package.json';
+import * as swStats from 'swagger-stats';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   app.enableCors();
+  app.use(swStats.getMiddleware({ swaggerSpec: document }));
+
   const { port } = app.get(AppConfig);
   await app.listen(port, '0.0.0.0');
 }
